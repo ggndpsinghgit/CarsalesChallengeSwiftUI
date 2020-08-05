d8be42542d97ff02c2b66b7ca44c1b434c522500
Gagandeep Singh <gagan.deep@me.com>
2020-08-05 12:34:30 +1000

    Initial Commit

---
+++ Shared/CarsalesChallengeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsalesChallengeSwiftUIApp.swift
+//  Shared
+//
+//  Created by Gagandeep Singh on 5/8/20.
+//
+
+import SwiftUI
+
+@main
+struct CarsalesChallengeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Gagandeep Singh on 5/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
